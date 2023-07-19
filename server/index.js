6071378046dabc703133eeb836a6d5e419cd8e52
Ayush-mega-coder <aayupurohit151@gmail.com>
2023-07-19 23:07:16 +0530

    add

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
-import express  from "express";
+// src/app.ts
+import express from 'express';
+import Connection from "./database/db.js";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express()
 const PORT = 8000
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD
 
+Connection(username, password);
 app.listen(PORT,()=>{
     console.log(`server running on ${PORT}`)
 } )
