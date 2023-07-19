6dd12e395592879a0032fcec257fc23b2d92f857
Lokesh Bansal <lokesh@75way.com>
2023-07-19 17:52:02 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Admin from './components/admin/Admin';
 
 function App() {
   return (
     <div className="App">
      <h1>MCQ QUIZ APP</h1>
      <p>Quiz app for MCQ</p>
+     <Admin/>
     </div>
   );
 }
---
+++ src/components/admin/Admin.tsx
@@ -0,0 +1,6 @@
+const Admin=()=>{
+    return(
+        <h1>Admin panel</h1>
+    )
+}
+export default Admin
